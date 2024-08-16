85e32df2932fc5782c072fc80677c6c3f4da903d
SAIMALK <aleesasaim496@gmail.com>
2024-08-16 23:05:12 +0500

    aad

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://md-backend.up.railway.app';
-export const STORYS_URL = 'https://md-backend.up.railway.app/api/story';
-export const UPLOAD_URL = 'https://md-backend.up.railway.app/api/upload'
-export const USERS_URL = 'https://md-backend.up.railway.app/api/users';
-export const AUTHOR_URL = 'https://md-backend.up.railway.app/api/author';
+export const STORYS_URL = '/api/story';
+export const UPLOAD_URL = '/api/upload'
+export const USERS_URL = '/api/users';
+export const AUTHOR_URL = '/api/author';
