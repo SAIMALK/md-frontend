6e240bfd172b4863c047b3529181964895996b2b
SAIMALK <aleesasaim496@gmail.com>
2024-08-16 22:47:59 +0500

    aab

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    port: 'https://md-frontend.up.railway.app', // or any port you prefer
+  },
   resolve: {
     alias: {
       '@': '/src', // Update according to your alias setup
