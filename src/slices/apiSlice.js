f74293620935f79c9039f68b5f20ae55e27eac9e
Shahzaib <shahzaib1234@outlook.com>
2024-10-17 11:48:37 +0500

    Fix(auth)

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 import { logout } from './authSlice';
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, 
+  credentials: 'include' });
+
 
 console.log(BASE_URL)
   
