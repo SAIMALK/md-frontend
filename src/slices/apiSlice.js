11b178e1308d80e29d9d270399e74cd017d94cb2
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2024-11-27 23:27:21 +0500

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -23,6 +23,6 @@ async function baseQueryWithAuth(args, api, extra) {
 
 export const apiSlice = createApi({
   baseQuery: baseQueryWithAuth, // Use the customized baseQuery
-    tagTypes: ['Story', 'User'],
+    tagTypes: ['Story', 'User','Author'],
     endpoints: (builder) => ({}),
 });
