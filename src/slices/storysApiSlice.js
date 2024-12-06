6c34f815efea4e4b978a2fa8259f7d92be6e2f77
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2024-12-06 10:33:37 +0500

    Update storysApiSlice.js

--- src/slices/storysApiSlice.js
+++ src/slices/storysApiSlice.js
@@ -13,26 +13,26 @@ export const storysApiSlice = apiSlice.injectEndpoints({
     }),
     getStoriesByAuthorId: builder.query({
       query: (authorId) => ({
-        url: ${STORYS_URL}/author/${authorId}, // Assuming there's an endpoint for fetching stories by author ID
+        url: `${STORYS_URL}/author/${authorId}`, // Assuming there's an endpoint for fetching stories by author ID
       }),
       keepUnusedDataFor: 5,
     }),
     getStoryDetails: builder.query({
       query: (storyId) => ({
-        url: ${STORYS_URL}/${storyId},
+        url: `${STORYS_URL}/${storyId}`,
       }),
       keepUnusedDataFor: 5,
     }),
     createStory: builder.mutation({
       query: () => ({
-        url: STORYS_URL,
+        url: ${STORYS_URL},
         method: 'POST',
       }),
       invalidatesTags: ['Story'],
     }),
     updateStory: builder.mutation({
       query: (data) => ({
-        url: ${STORYS_URL}/${data.storyId},
+        url: `${STORYS_URL}/${data.storyId}`,
         method: 'PUT',
         body: data,
       }),
@@ -40,21 +40,21 @@ export const storysApiSlice = apiSlice.injectEndpoints({
     }),
     uploadStoryCover: builder.mutation({
       query: (data) => ({
-        url: ${UPLOAD_URL},
+        url: `${UPLOAD_URL}`,
         method: 'POST', 
         body: data,
       }),
     }),
     deleteStory: builder.mutation({
       query: (storyId) => ({
-        url: ${STORYS_URL}/${storyId},
+        url: `${STORYS_URL}/${storyId}`,
         method: 'DELETE',
       }),
       providesTags: ['Story'],
     }),
     createReview: builder.mutation({
       query: (data) => ({
-        url: ${STORYS_URL}/${data.storyId}/reviews,
+        url: `${STORYS_URL}/${data.storyId}/reviews`,
         method: 'POST',
         body: data,
       }),
@@ -62,14 +62,14 @@ export const storysApiSlice = apiSlice.injectEndpoints({
     }),
     deleteComment: builder.mutation({
       query: ({ storyId, commentId }) => ({
-        url: ${STORYS_URL}/${storyId}/reviews/${commentId},
+        url: `${STORYS_URL}/${storyId}/reviews/${commentId}`,
         method: "DELETE",
   }),
   invalidatesTags: ["Story"],
 }),
 
     getTopStorys: builder.query({
-      query: () => ${STORYS_URL}/top,
+      query: () => `${STORYS_URL}/top`,
       keepUnusedDataFor: 5,
     }),
   }),
