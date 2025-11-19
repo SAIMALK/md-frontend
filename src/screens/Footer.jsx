6a5f9fcfffa8e57494417ff8c9c299657ea16820
SAIMALK <115073479+SAIMALK@users.noreply.github.com>
2025-11-19 09:57:12 +0500

    Remove HubSpot embed code from Footer
    Removed HubSpot embed code from Footer component.

--- src/screens/Footer.jsx
+++ src/screens/Footer.jsx
@@ -21,7 +21,5 @@ function Footer() {
           </>
   )
 }
-<!-- Start of HubSpot Embed Code -->
-  <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/20570446.js"></script>
-<!-- End of HubSpot Embed Code -->
+
 export default Footer
